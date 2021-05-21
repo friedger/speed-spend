e8888dcfbd96b8ced8f467126414462c012e2fe9
Friedger Müffke <mail@friedger.de>
2021-05-21 23:01:14 +0200

    more pre checks

--- src/components/BtcRewardTxSubmission.js
+++ src/components/BtcRewardTxSubmission.js
@@ -1,6 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react';
 
-import { blocksApi, CONTRACT_ADDRESS, NETWORK, POOL_AUDIT_CONTRACT_NAME } from '../lib/constants';
+import {
+  blocksApi,
+  CLARITY_BITCOIN_CONTRACT_NAME,
+  CONTRACT_ADDRESS,
+  NETWORK,
+  POOL_AUDIT_CONTRACT_NAME,
+} from '../lib/constants';
 import { TxStatus } from '../lib/transactions';
 import { fetchAccount } from '../lib/account';
 import { useConnect as useStacksJsConnect } from '@stacks/connect-react';
@@ -60,9 +66,9 @@ export function BtcRewardTxSubmission({ ownerStxAddress, userSession }) {
       await fetch(`https://api.blockcypher.com/v1/btc/test3/blocks/${tx.block_hash}?limit=500`)
     ).json();
     const txIndex = block.txids.findIndex(id => id === txId);
-    const stxBlocks = blocksApi.getBlockList({ limit: 100, offset: 0 });
+    const stxBlock = await blocksApi.getBlockByHeight({ height });
 
-    console.log({ tx, block, txIndex });
+    console.log({ txId, tx, block, txIndex });
     const tree = new MerkleTree(block.txids, SHA256, { isBitcoinTree: true });
     console.log(tree.getProof(txId).map(po => po.data.toString('hex')));
     console.log(tree.getProof(txId).map(po => reverse(po.data).toString('hex')));
@@ -94,24 +100,29 @@ export function BtcRewardTxSubmission({ ownerStxAddress, userSession }) {
     const functionArgs = [blockCV, txCV, proofCV];
     try {
       setStatus(`Sending transaction`);
-      let check = await callReadOnlyFunction({
-        contractAddress,
-        contractName: 'clarity-bitcoin-v2',
-        functionName: 'verify-merkle-proof',
-        functionArgs: [
-          bufferCV(reverse(MerkleTree.bufferify(txId))),
-          bufferCV(reverse(MerkleTree.bufferify(block.mrkl_root))),
-          proofCV,
-        ],
-        senderAddress: contractAddress,
-        network: NETWORK,
-      });
-      console.log('verify-merkle-proof', JSON.stringify(check));
+      let check;
 
+      try {
+        check = await callReadOnlyFunction({
+          contractAddress,
+          contractName: CLARITY_BITCOIN_CONTRACT_NAME,
+          functionName: 'verify-merkle-proof',
+          functionArgs: [
+            bufferCV(reverse(MerkleTree.bufferify(txId))),
+            bufferCV(reverse(MerkleTree.bufferify(block.mrkl_root))),
+            proofCV,
+          ],
+          senderAddress: contractAddress,
+          network: NETWORK,
+        });
+        console.log('verify-merkle-proof', JSON.stringify(check));
+      } catch (e) {
+        console.log('verify-merkle-proof', e);
+      }
       // was mined?
       check = await callReadOnlyFunction({
         contractAddress,
-        contractName: 'clarity-bitcoin-v2',
+        contractName: CLARITY_BITCOIN_CONTRACT_NAME,
         functionName: 'was-tx-mined?',
         functionArgs,
         senderAddress: contractAddress,
@@ -119,47 +130,171 @@ export function BtcRewardTxSubmission({ ownerStxAddress, userSession }) {
       });
       console.log('was-tx-mined', JSON.stringify(check));
 
-      // verify header
+      // parse block header
       check = await callReadOnlyFunction({
         contractAddress,
-        contractName: 'clarity-bitcoin-v2',
+        contractName: CLARITY_BITCOIN_CONTRACT_NAME,
         functionName: 'parse-block-header',
         functionArgs: [bufferCV(Buffer.from(blockHeader, 'hex'))],
         senderAddress: contractAddress,
         network: NETWORK,
       });
-      console.log('parse', cvToString(check.value.data['merkle-root']));
+      console.log('parse block header', cvToString(check.value.data['merkle-root']));
 
       // verify header
       check = await callReadOnlyFunction({
         contractAddress,
-        contractName: 'clarity-bitcoin-v2',
+        contractName: CLARITY_BITCOIN_CONTRACT_NAME,
         functionName: 'verify-block-header',
-        functionArgs: [bufferCV(Buffer.from(blockHeader, 'hex')), uintCV(11319)],
+        functionArgs: [
+          bufferCV(
+            Buffer.from(parts[0] + parts[1] + parts[2] + parts[3] + parts[4] + parts[5], 'hex')
+          ),
+          uintCV(11319),
+        ],
         senderAddress: contractAddress,
         network: NETWORK,
       });
-      console.log('verify-header', cvToString(check));
+      console.log('verify-header', JSON.stringify(check));
 
       // parse tx
-      /*
+      try {
+        check = await callReadOnlyFunction({
+          contractAddress,
+          contractName: CLARITY_BITCOIN_CONTRACT_NAME,
+          functionName: 'parse-tx',
+          functionArgs: [txCV],
+          senderAddress: contractAddress,
+          network: NETWORK,
+        });
+        console.log('parse-tx', cvToString(check));
+      } catch (e) {
+        console.log('parse-tx failed', e.toString());
+      }
+
+      // check was mined v2
+      const blockParts = tupleCV({
+        version: bufferCV(Buffer.from(parts[0], 'hex')),
+        parent: bufferCV(Buffer.from(parts[1], 'hex')),
+        'merkle-root': bufferCV(Buffer.from(parts[2], 'hex')),
+        timestamp: bufferCV(Buffer.from(parts[3], 'hex')),
+        nbits: bufferCV(Buffer.from(parts[4], 'hex')),
+        nonce: bufferCV(Buffer.from(parts[5], 'hex')),
+        height: uintCV(height),
+      });
+      console.log(blockParts.data.nonce.buffer.toString('hex'));
+
       check = await callReadOnlyFunction({
         contractAddress,
-        contractName: 'clarity-bitcoin-v2',
-        functionName: 'parse-tx',
-        functionArgs: [txCV],
+        contractName: CLARITY_BITCOIN_CONTRACT_NAME,
+        functionName: 'was-tx-mined-2?',
+        functionArgs: [blockParts, txCV, proofCV],
         senderAddress: contractAddress,
         network: NETWORK,
       });
-      console.log('parse-tx', cvToString(check));
-      */
+      console.log('was-tx-mined-2?', cvToString(check));
 
       // submit
       await doContractCall({
         contractAddress,
         contractName,
         functionName: 'submit-reward-tx',
-        functionArgs,
+        functionArgs: [
+          // block
+          blockParts,
+          // tx
+          tupleCV({
+            version: bufferCV(Buffer.from('01000000', 'hex')),
+            ins: listCV([
+              tupleCV({
+                outpoint: tupleCV({
+                  hash: bufferCV(
+                    Buffer.from(
+                      'c8bd3502a21f810da7692e323cc46e0e9ec1def7a93cc610f6d65b60193174e2',
+                      'hex'
+                    )
+                  ),
+                  index: bufferCV(Buffer.from('03000000', 'hex')),
+                }),
+                scriptSig: bufferCV(
+                  Buffer.from(
+                    '47304402204ffe267e6b5aab28350be80c1f4ea94424c483f3f44f175594bb6273000f80e8022042ebd5668420c8b29d2ec2791e2c8aa0d7784d8a6283f958fe581e0be129c61b0121037435c194e9b01b3d7f7a2802d6684a3af68d05bbf4ec8f17021980d777691f1d',
+                    'hex'
+                  )
+                ),
+                sequence: bufferCV(Buffer.from('fdffffff', 'hex')),
+              }),
+            ]),
+            outs: listCV([
+              tupleCV({
+                scriptPubKey: bufferCV(
+                  Buffer.from(
+                    '6a4c5058365b13588072c8b4eca88a505db5c453123c5c91db98d90ac1cd124402dba596531ebf945361dbdbcb0a43e8d6984ab8eee14982d0341eab198fc74d2d917c6d95dc001e21c20008001e1fc2001d02',
+                    'hex'
+                  )
+                ),
+                value: bufferCV(Buffer.from('0000000000000000', 'hex')),
+              }),
+              tupleCV({
+                scriptPubKey: bufferCV(
+                  Buffer.from('76a914c70e1ca5a5ef633fe5464821ca421c173997f38888ac', 'hex')
+                ),
+                value: bufferCV(Buffer.from('1027000000000000', 'hex')),
+              }),
+              tupleCV({
+                scriptPubKey: bufferCV(
+                  Buffer.from('76a9146c575e9f31715b180b22738136895876ade678cb88ac', 'hex')
+                ),
+                value: bufferCV(Buffer.from('1027000000000000', 'hex')),
+              }),
+              tupleCV({
+                scriptPubKey: bufferCV(
+                  Buffer.from('76a914ba27f99e007c7f605a8305e318c1abde3cd220ac88ac', 'hex')
+                ),
+                value: bufferCV(Buffer.from('752f7c5c00000000', 'hex')),
+              }),
+            ]),
+            locktime: bufferCV(Buffer.from('00000000', 'hex')),
+          }),
+
+          // proof
+          tupleCV({
+            'tx-index': uintCV(6),
+            hashes: listCV([
+              bufferCV(
+                Buffer.from(
+                  '3ae3dfeedc6eb99fb5e2c5d0c90697a66de969c3f4d974ebe2ef104fcea7f13b',
+                  'hex'
+                )
+              ),
+              bufferCV(
+                Buffer.from(
+                  '52500d11cabf1049ebb139a82b439d08bd3a8e867a41fb3f368dfa125e043989',
+                  'hex'
+                )
+              ),
+              bufferCV(
+                Buffer.from(
+                  'a104c2725aabf28fcf3c304fd370610370330c546495acd5015ecc177c6494f6',
+                  'hex'
+                )
+              ),
+              bufferCV(
+                Buffer.from(
+                  '5e4442a235be2fc92aa15ba3b59c5af61c46dff8e7ed8198ebc48ec6d71a6a49',
+                  'hex'
+                )
+              ),
+              bufferCV(
+                Buffer.from(
+                  '904640bdf50c8edd12232efc41966a3a9af955208b205a90fc8a6dca5f69c458',
+                  'hex'
+                )
+              ),
+            ]),
+            'tree-depth': uintCV(5),
+          }),
+        ],
         postConditionMode: PostConditionMode.Deny,
         postConditions: [],
         userSession,
@@ -186,6 +321,7 @@ export function BtcRewardTxSubmission({ ownerStxAddress, userSession }) {
         <input
           type="number"
           ref={heightRef}
+          defaultValue="11319"
           className="form-control"
           onKeyUp={e => {
             if (e.key === 'Enter') txIdRef.current.focus();
@@ -200,8 +336,9 @@ export function BtcRewardTxSubmission({ ownerStxAddress, userSession }) {
           type="text"
           ref={txIdRef}
           className="form-control"
+          defaultValue="7ad9187efd4fa01ce8690015a1a711d7958f18c248fb4c47a32d00732cfc4a61"
           onKeyUp={e => {
-            if (e.key === 'Enter') lockingPeriod.current.focus();
+            if (e.key === 'Enter') submitAction();
           }}
           onBlur={e => {
             setStatus(undefined);
--- src/lib/constants.js
+++ src/lib/constants.js
@@ -31,7 +31,8 @@ export const ROCKET_MARKET_CONTRACT_NAME = 'rocket-market-v3';
 export const ROCKET_FACTORY_CONTRACT_NAME = 'rocket-factory-v5';
 export const ROCKET_TOKEN_CONTRACT_NAME = 'rocket-token';
 export const POOL_REGISTRY_CONTRACT_NAME = 'pool-registry-v2';
-export const POOL_AUDIT_CONTRACT_NAME = 'pool-audit-v3';
+export const POOL_AUDIT_CONTRACT_NAME = 'pool-audit-v6';
+export const CLARITY_BITCOIN_CONTRACT_NAME = "clarity-bitcoin-v4"
 export const GENESIS_CONTRACT_ADDRESS = 'ST000000000000000000002AMW42H';
 export const BNS_CONTRACT_NAME = 'bns';
 
